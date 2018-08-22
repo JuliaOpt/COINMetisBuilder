5cf304c167b6c416d57d3da1f7a50d6e23db1085
Juan Pablo Vielma <jvielma@mit.edu>
2018-08-22 16:11:12 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "MetisBuilder"
+name = "COINMetisBuilder"
 version = v"1.3.5"
 
 # Collection of sources required to build MetisBuilder
